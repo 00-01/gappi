cb2be042670539d359f6a83f46c20ff809d366f0
00-01 <0.00.001.001a@gmail.com>
2022-04-12 18:15:04 +0900

    .

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ with open('device_id.txt') as f:
 v1 = ["01", "03", "04", "05", "06"]
 
 if device_id in v1:
-    import main.v1
+    from version import v1
     # v1()
 else:
-    import main.v2
+    from version import v2
     # v2()
