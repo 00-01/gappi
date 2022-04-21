50f3faa9cf5ba992142fa2bbe3ec968635f66d0e
00-01 <0.00.001.001a@gmail.com>
2022-04-21 13:02:50 +0900

    .

--- command.sh
+++ command.sh
@@ -1,16 +1,11 @@
 crontab cron
 
-pip3 install opencv-contrib-python
-
+#pip3 install opencv-contrib-python
 #pip3 install -U pip
 #sudo apt install gfortran libopenblas-dev liblapack-dev libhdf5-dev
 #sudo pip3 install setuptools wheel h5py
-
-
-
 #pip3 install tensorflow
 #pip3 install matplotlib
-
 
 #id='02'
 #file='device_id.txt'
