1da1fd170016af6ebe8675a3788d6c313ebcc67a
00-01 <59557934+00-01@users.noreply.github.com>
2022-10-03 09:17:14 +0900

    Update autorun.sh
    .

--- autorun.sh
+++ autorun.sh
@@ -1,12 +1,12 @@
 #! /bin/zsh
 
 case "$(pidof python3 | wc -w)" in
-0)  echo "Restarting python3: $(date)" >> log/run.log
-    python3 gappi/main_v3.py >> log/main.log
-    ;;
-1)  # all ok
-    ;;
-*)  echo "Removed double python3: $(date)" >> log/run.log
-    kill $(pidof python3 | awk '{print $1}')
-    ;;
+    0)  echo "Restarting python3: $(date)" >> log/run.log
+        python3 gappi/main_v3.py >> log/main.log
+        ;;
+    1)  # running normally 
+        ;;
+    *)  echo "Removed double python3: $(date)" >> log/run.log
+        kill $(pidof python3 | awk '{print $1}')
+        ;;
 esac
