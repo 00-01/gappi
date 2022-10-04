a484060665d8b550b456a2e45fb0567ce120da86
00-01 <0.00.001.001a@gmail.com>
2022-10-05 02:43:36 +0900

    .

--- scipt/check.sh
+++ scipt/check.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 cd ~/gappi
+
 git remote update
 
 UPSTREAM=${1:-'@{u}'}
@@ -10,15 +11,19 @@ BASE=$(git merge-base @ "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
     echo "0: PASS"
-elif [ $LOCAL = $BASE ]; then
+#elif [ $LOCAL = $BASE ]; then
+#    echo "1: PULLING"
+#    cd ~/gappi && git reset --hard && echo "[PULL] $(date)" >> ~/log/git.log && git pull >> ~/log/git.log && cd
+#    crontab ~/gappi/script/cron
+#    cp -f ~/gappi/script/.zshrc ~/.zshrc
+#    sudo reboot now
+#elif [ $REMOTE = $BASE ]; then
+#    echo "2: PUSH NEED"
+else
     echo "1: PULLING"
     cd ~/gappi && git reset --hard && echo "[PULL] $(date)" >> ~/log/git.log && git pull >> ~/log/git.log && cd
-    sudo reboot now
-elif [ $REMOTE = $BASE ]; then
-    echo "2: PUSH NEED"
-else
-    echo "3: ETC"
-    cd ~/gappi && git reset --hard && echo "[PULL] $(date)" >> ~/log/git.log && git pull >> ~/log/git.log && cd
+    crontab ~/gappi/script/cron
+    cp -f ~/gappi/script/.zshrc ~/.zshrc
     sudo reboot now
 fi
 
