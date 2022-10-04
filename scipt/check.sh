52fd02de3b97d5ff7a20d43095c22fd7a25bfa55
00-01 <0.00.001.001a@gmail.com>
2022-10-05 02:16:01 +0900

    .

--- scipt/check.sh
+++ scipt/check.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+cd ~/gappi
 git remote update
 
 UPSTREAM=${1:-'@{u}'}
@@ -12,9 +13,13 @@ if [ $LOCAL = $REMOTE ]; then
 elif [ $LOCAL = $BASE ]; then
     echo "1: PULLING"
     cd ~/gappi && git reset --hard && echo "[PULL] $(date)" >> ~/log/git.log && git pull >> ~/log/git.log && cd
-    rb
+    sudo reboot now
 elif [ $REMOTE = $BASE ]; then
     echo "2: PUSH NEED"
 else
     echo "3: ETC"
+    cd ~/gappi && git reset --hard && echo "[PULL] $(date)" >> ~/log/git.log && git pull >> ~/log/git.log && cd
+    sudo reboot now
 fi
+
+cd
