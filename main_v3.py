1975d42c2b5dff02eef6f5ebf23d14b6aed49d41
00-01 <0.00.001.001a@gmail.com>
2022-10-07 19:35:37 +0900

    .

--- main_v3.py
+++ main_v3.py
@@ -361,8 +361,9 @@ def poster():
     # files = {"ir_image": (ir_path, open(ir_path, 'rb'), 'image/png'),
              "rgb_image": open(rgb_path, 'rb'),
              # "fg_image": (fg_path, open(fg_path, 'rb'), 'image/png'),
-             # "log": open(log, 'rb'),
+             "log": open(log, 'rb'),
              }
+    log.close()
     log
     r = post(url, data=data, files=files)
     # print(r.headers, file=log)
